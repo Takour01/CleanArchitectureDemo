f6f38f79503589d3fb30d76affd1acce0dbf66f5
Takour01 <azerpoi675@gmail.com>
2024-09-12 16:14:52 +0200

    init

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configuration if needed
+        }
+    }
+}
---
+++ Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Application.Services;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _productService.GetAllProductsAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Product> Get(int id)
+        {
+            return await _productService.GetProductByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Product product)
+        {
+            await _productService.AddProductAsync(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Product product)
+        {
+            product.Id = id;
+            await _productService.UpdateProductAsync(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _productService.DeleteProductAsync(id);
+        }
+    }
+}
